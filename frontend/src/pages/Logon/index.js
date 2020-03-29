3ae41c2aac0bf6dcff571740c361dda5530dc9fd
mavis42 <victor_42matheus@outlook.com>
2020-03-29 19:42:02 -0300

    front-end do app

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
+import {FiLogIn} from 'react-icons/fi';
+
+import api from '../../services/api'
+
+import './style.css';
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png';
+
+export default function Logon(){
+    const [id, setId] = useState('');
+    const history = useHistory('');
+
+    async function handlelogin(e) {
+        e.preventDefault();
+
+        try{    
+            const response = await api.post('sessions', { id });
+
+          localStorage.setItem('ongId', id);
+          localStorage.setItem('ongName', response.data.name);
+
+          history.push('/profile');
+        } catch(err) {
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+
+                <form onSubmit={handlelogin}>
+                    <h1>faça seu logon</h1>
+
+                    <input
+                     placeholder="Sua ID"
+                     value = {id}
+                     onChange={e => setId(e.target.value)}
+                     />
+                    <button className="button" type="submit">ENTRAR</button>
+                    
+                    <Link className="black-link"to="/register">
+                        <FiLogIn size={16} color="#e02041"/>
+                        não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+
+             <img src={heroesImg} alt="heroes" />
+
+        </div>
+        
+    );
+
+}
---
+++ frontend/src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncidents from './pages/NewIncidents';
+
+
+
+
+export default function routes() {
+    return (
+        <BrowserRouter>
+       <Switch>
+           <Route path="/" exact component={Logon}/>
+           <Route path="/register" component={Register}/>
+
+           <Route path="/profile" component={Profile}/>
+           <Route path="/incidents/new" component={NewIncidents}/>
+       </Switch>
+       </BrowserRouter>  
+    )
+}
+
+
